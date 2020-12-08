f151db21bd8c4cf28373b94fdfe55a0595506208
Anthony Lesch <23031998+GutterBeats@users.noreply.github.com>
2020-12-07 22:19:00 -0800

    Update BullCowCartridge.cpp
    Added welcome message for player.

--- Section 3/BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
+++ Section 3/BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,15 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    PrintLine("Welcome to the BullCow Game!");
+    PrintLine("Please type your name and press enter.");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    ClearScreen();
+    FString WelcomeMessage = "Welcome " + Input + ", let's play!";
 
+    PrintLine(WelcomeMessage);
 }
