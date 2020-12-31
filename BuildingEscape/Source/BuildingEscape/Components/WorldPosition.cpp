e99414a55199e16434b03af2e8aefaa0e2157628
Anthony Lesch <23031998+GutterBeats@users.noreply.github.com>
2020-12-31 15:27:51 -0800

    Added files.

--- BuildingEscape/Source/BuildingEscape/Components/WorldPosition.cpp
+++ BuildingEscape/Source/BuildingEscape/Components/WorldPosition.cpp
@@ -1,6 +1,7 @@
 // Copyright Anthony Lesch 2020
 
 #include "WorldPosition.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UWorldPosition::UWorldPosition()
@@ -8,8 +9,6 @@ UWorldPosition::UWorldPosition()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -18,8 +17,23 @@ void UWorldPosition::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	// FString Log = "Hello!";
+	// FString* PrtLog = &Log;
+	//
+	// UE_LOG(LogTemp, Warning, TEXT("%s"), *Log);
+	// UE_LOG(LogTemp, Warning, TEXT("%s"), **PrtLog);
+
+	const AActor* Owner = GetOwner();
+	if (!IsValid(Owner))
+	{
+		return;
+	}
 	
+	const FString ObjectName = Owner->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("This component belongs to %s"), *ObjectName);
+
+	const FString ObjectPositionString = Owner->GetActorLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("The %s is at the position %s."), *ObjectName, *ObjectPositionString);
 }
 
 
@@ -27,7 +41,5 @@ void UWorldPosition::BeginPlay()
 void UWorldPosition::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
 }
 
